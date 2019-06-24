713c7ab5c08765f62a53eee34ced60cfaacc1e3d
Dhouha Mansour <dhouha.mansour11@gmail.com>
2019-06-24 22:10:02 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,3 @@
-/* 
-  
-FizzBuzz Algorithm 
-  
-A function that takes 1 parameter (num) and log out to the console all the numbers from 1 to num 
-For each number, if the number is divisible by 3 => log Fizz 
-if the number is divisible by 5 => log Buzz 
-if the number is divisible by 3 and 5  => log FizzBuzz 
-  
-*/ 
-  
 function fizzbuzz (num){ 
   for (var i=1; i<= num; i++) { 
     if ((i % 3 === 0 ) && (i % 5 === 0 )) { 
